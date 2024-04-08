b05ccd63b623090ab017e39e28a950560a47bbe6
Kimnabin <ducvucong01@gmail.com>
2024-04-08 17:58:41 +0900

    Redux persist de luu tru store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,33 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import userReducer from './userSlice';
-export default configureStore({
-    reducer: {
-        auth: authReducer,
-        users : userReducer,
-    },
-});
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+const rootReducer = combineReducers({ auth : authReducer, users : userReducer })   // Combine all the reducers
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export let persistor = persistStore(store)
